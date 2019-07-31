8d6768cba470ff6fd7453c1b7716916ee5813b18
AlienSV <51966211+AlienSV@users.noreply.github.com>
2019-07-31 20:11:33 +0300

    сделано задание к 3-му уроку

--- js/code.js
+++ js/code.js
@@ -1,67 +1,69 @@
 'use strict';
-
 //задание 1
-//пример 1. Выводит 2 потому что сначала выполняется префиксный инкремент, а потом присваивание
-//пример 2. Выводит 1 потому что постфиксный инкремент выполняется после присваивания
-//пример 3. После первого примера в c лежит 2, увеличивается на единицу и складывается с 2, ответ 5
-//пример 4. b = 2 после примера 2, постфиксная форма инкремента не повлияет на результат текущего выражения, ответ 4
-//a = 1 в начале, дважды увеличивалось на 1, после примеров 1 и 3, результат 3
-//b = 1 в начале, дважды увеличивалось на 1, после примеров 2 и 4, результат 3
+let a = [
+ [{
+        name: 'Mary',
+        age: 23
+    }, {
+        name: 'Mike',
+        age: 45
+    }, {
+        name: 'Nick',
+        age: 11
+    }],
+ [{
+        name: 'Adam',
+        age: 56
+    }, {
+        name: 'Sara',
+        age: 21
+    }, {
+        name: 'Don',
+        age: 22
+    }],
+ [{
+        name: 'Karl',
+        age: 34
+    }, {
+        name: 'Marta',
+        age: 76
+    }, {
+        name: 'John',
+        age: 19
+    }]
+];
+
+let copy = JSON.parse(JSON.stringify(a));
+//copy[0][0].name = "Sergey"; проверка копирования по значению
+//console.log(a);
+console.log(copy);
 
 //задание 2
-//a будет равно 5
+let basket = [];
+//заполняем случайными значениями
+for (let i = 1; i <= 10; i++) {
+    basket.push({
+        art: Math.round(Math.random() * 1e6),
+        name: 'product_' + i,
+        price: Math.round(Math.random() * 1e3 + 1e3)
+    });
+}
+//считаем общую стоимость
+function countBasketPrice() {
+    let total = 0;
+    basket.forEach(function (item) {
+        total += item.price;
+    });
+    return total;
+}
+console.log(`Стоимость товаров в корзине - ${countBasketPrice()} руб.`);
 
 //задание 3
-let a = 4,
-    b = -2;
-if (a >= 0 && b >= 0) console.log(a - b);
-if (a < 0 && b < 0) console.log(a * b);
-if ((a >= 0 && b < 0) || (a < 0 && b >= 0)) console.log(a + b);
+for (let i = 0; i < 9; i++, console.log(i)) {};
 
 //задание 4
-var add = (a, b) => {
-    return a + b; // пришлось так делать, потому что по заданию использование return обязательно
-}
-var sub = (a, b) => {
-    return a - b;
-}
-var mult = (a, b) => {
-    return a * b;
-}
-var div = (a, b) => {
-    return a / b;
-}
-
-//задание 5
-function mathOperation(arg1, arg2, operation) {
-    switch (operation) {
-        case "+":
-            return add(arg1, arg2);
-        case "-":
-            return sub(arg1, arg2);
-        case "*":
-            return mult(arg1, arg2);
-        case "/":
-            return div(arg1, arg2);
-        default:
-            alert("Неверная операция");
-    }
-}
-
-//задание 6
-function power(val, pow) {
-    if (pow != 1) {
-        return val * power(val, pow - 1);
-    } else {
-        return val;
-    }
-}
-
-//задание 7
-function parsePassValidity(path) {
-    let spSymParse = (path.indexOf("$") != -1) || (path.indexOf("%") != -1) || (path.indexOf("*") != -1) || (path.indexOf("&") != -1);
-    let lenParse = (path.length >= 3) && (path.length <= 12);
-    let qzParse = (path[0] == "q") || (path.lastIndexOf("z") == path.length - 1);
-    if (spSymParse && lenParse && qzParse) return "Корректный пароль";
-    return "Некорректный пароль";
+for (let i = 1; i <= 20; i++) {
+    let a = '';
+    for (let j = 1; j <= i; j++) a += '*';
+    console.log(a);
 }
